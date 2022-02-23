5598808c3ffcc7a30becd5e2309a88d945f6abee
wgcotera <wgcotera@espol.edu.ec>
2022-02-23 03:39:28 -0500

    Home Page: Header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAvVZ7t5DOJE8UntS0nbaPFpcuFMNB3B1Y",
+    authDomain: "clone-f5772.firebaseapp.com",
+    projectId: "clone-f5772",
+    storageBucket: "clone-f5772.appspot.com",
+    messagingSenderId: "169873278691",
+    appId: "1:169873278691:web:8a85996a6f96612697a7d6",
+    measurementId: "G-T7C7N3N2FL"
+  };
