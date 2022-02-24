b68b45b8497b0fc2cd1328af608cda9b6cd89e62
wgcotera <wgcotera@espol.edu.ec>
2022-02-24 16:40:13 -0500

    Home Page: Body

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
-import Header from './Header';
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
     // Following the BEM convention for naming
     <div className="app">
       {/* Header */}
-      <Header/>
+      <Header />
       {/* Home */}
+      <Home />
     </div>
   );
 }
