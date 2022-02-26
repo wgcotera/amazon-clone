8090ef25c74c1f87643a78a93663bad7dd448acb
wgcotera <wgcotera@espol.edu.ec>
2022-02-26 04:31:21 -0500

    Checkout Page: Done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Checkout from "./Checkout";
 
 function App() {
   return (
     // Following the BEM convention for naming
-    <div className="app">
-      {/* Header */}
-      <Header />
-      {/* Home */}
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
