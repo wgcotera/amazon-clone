170df6f477a179885644ffe50197f7a008c406ea
wgcotera <wgcotera@espol.edu.ec>
2022-03-01 22:31:11 -0500

    Payment Page: Done

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,8 +3,11 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./reducer";
+// useNavigate from react v6 replaces useHistory from react v5
+import { useNavigate } from "react-router-dom";
 
 function Subtotal() {
+  const navigate = useNavigate();
   const [{ basket }, dispatch] = useStateValue();
 
   return (
@@ -26,7 +29,10 @@ function Subtotal() {
         thousandSeparator={true}
         prefix={"$"}
       />
-      <button>Proceed to Checkout</button>
+      <button onClick={(e) => navigate("/payment")}>
+        {" "}
+        Proceed to Checkout
+      </button>
     </div>
   );
 }
