68ccb8b6e9cafe3ed47e1124f6845b6dd3cd12af
wgcotera <wgcotera@espol.edu.ec>
2022-03-05 00:32:21 -0500

    Treating warnings as errors: Fixed

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Home from "./Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Checkout from "./Checkout";
 import Login from "./Login";
+import Payment from "./Payment";
+import Orders from "./Orders";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
-import Payment from "./Payment";
 import { loadStripe } from "@stripe/stripe-js";
-import { Elements, from } from "@stripe/react-stripe-js";
-import Orders from "./Orders";
+import { Elements } from "@stripe/react-stripe-js";
 
 const promise = loadStripe(
   "pk_test_51KYjSqDG1nZipsBwHm2vYfIOQRmyUhJML8lwUPptNnVQFBQ6giOIECrdjCJjziyLkWm6ILkhe5l3NZRbZ1T13B8400khBg0PQU"
